0a68ff658dcef75437743aa9085b7eea8aa8fa98
TheElephantCoder <157982101+TheElephantCoder@users.noreply.github.com>
2025-01-06 13:46:47 -0500

    Added basic Hello World program

---
+++ Pi-Hole Manager/Pi_Hole_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pi_Hole_ManagerApp.swift
+//  Pi-Hole Manager
+//
+//  Created by Mohammed, Abdul Majid on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Pi_Hole_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pi-Hole ManagerTests/Pi_Hole_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pi_Hole_ManagerTests.swift
+//  Pi-Hole ManagerTests
+//
+//  Created by Mohammed, Abdul Majid on 1/5/25.
+//
+
+import Testing
+@testable import Pi_Hole_Manager
+
+struct Pi_Hole_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
